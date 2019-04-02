5b15bdd9fdbe567d7a997f6275780a361ec81910
jabris16 <49063283+jabris16@users.noreply.github.com>
2019-04-02 14:26:03 +0100

    Update medi_track.py

--- medi_track.py
+++ medi_track.py
@@ -17,6 +17,9 @@ pressure_treshold = []    # pressure gradient of feature point vs. all surroudin
 wind_threshold = []    # average wind-speed value used as minimum threshold for detection
 precip_threhsold = []   # average precipitation value used minimum threshold for detection
 
+# miscellaneous 
+precip_per_day_calc = 86400     # multiplication factor to later convert precipitation volume variable (mm) to precipitation volume per day variable (mm/day)
+
 #************************************************************************************************************
 # read in file and establish variables
 
@@ -24,7 +27,7 @@ nc = Dataset(filename)
 mslp = nc.variables['x'][:]    # MSLP variable where x = MSLP header name in filename ncdf 
 bg_mslp = np.mean(mslp)     # background MSLP variable
 anomaly = np.subtract(mslp,bg_mslp)    # MSLP anomaly variable
-precip = nc.variables['a'][:]    # precipitation variable where a = precipitation header name in filename ncdf 
+precip = nc.variables['a'][:] * precip_per_day_calc   # precipitation volume per day variable (including conversion multiplication factor) where a = precipitation header name in filename ncdf 
 wind = nc.variables['b'][:]    # wind-speed variable where b = wind-speed header name in filename ncdf 
 time = nc.variables['c'][:]    # time variable where c = time header name in filename ncdf 
 lat = nc.variables['y'][:]    # latitude variable where y = latitude header name in filename ncdf 
@@ -54,10 +57,11 @@ wind_list = []
 #************************************************************************************************************
 # loop through all data points across space and time to establish medicane feature points and store in dataframe
 
-for i in xrange(anomaly_shape[0]):
-    for j in xrange(1,anomaly_shape[1]-1):    # first and last points excluded to account for boundaries of domain
-        for k in xrange(1,anomaly_shape[2]-1):    # first and last points exluded to account for boundaries of domain
-            # to assess negative gradient feature points only and that feature pointvs adjacent cells gradient exceeds defined pressure gradient threhsold
+for i in xrange(anomaly_shape[0]):      # loop through all time steps
+    for j in xrange(1,anomaly_shape[1]-1):    # loop through all latitudes; first and last points excluded to account for spatial boundaries of domain
+        for k in xrange(1,anomaly_shape[2]-1):    # loop through all longitudes; first and last points exluded to account for spatial boundaries of domain
+            
+            # to assess negative gradient feature points only and that feature pointvs adjacent cells gradient exceeds defined pressure gradient threhsold:
             if anomaly[i,j,k] < 0 and \  
             anomaly[i,j+1,k] - anomaly[i,j,k] > pressure_threshold and \
             anomaly[i,j,k+1] - anomaly[i,j,k] > pressure_threshold and \
@@ -68,22 +72,23 @@ for i in xrange(anomaly_shape[0]):
                 pass
             
             if ident[i,j,k] == 1:     # storing feature point adjacent precipitation and wind-speed values
-                precip_adj_total[i,j,k] = precip[i,j+1,k]
+                precip_adj_total[i,j,k] = precip[i,j+1,k]     
             + precip[i,j,k+1]
             + precip[i,j-1,k]
             + precip[i,j,k-1]
-            precip_adj_av[i,j,k] = precip_adj_total[i,j,k]/4
+            precip_adj_av[i,j,k] = precip_adj_total[i,j,k]/4        # calculating average precipitation rates in cells adjacent to feature point
                 wind_adj_total[i,j,k] = wind[i,j+1,k]
             + wind[i,j,k+1]
             + wind[i,j-1,k]
             + wind[i,j,k-1]
-            wind_adj_av[i,j,k] = wind_adj_total[i,j,k]/4
+            wind_adj_av[i,j,k] = wind_adj_total[i,j,k]/4        # calculating average wind speed in cells adjacent to feature point
             
             if precip_adj_av[i,j,k] > precip_threshold and \
             wind_adj_av[i,j,k] > wind_threshold:    # identified feature point characteristics which exceed defined thresholds stored in lists before being converted to numpy arrays
-                lat_list.append(lat[j])
-                lat_store = np.array(lat_list)
-                lon_list.append(lon[k])
+                
+                lat_list.append(lat[j])     # append list to add feature point characteristic
+                lat_store = np.array(lat_list)      # convert list to np array
+                lon_list.append(lon[k])             # repeat process for all other characteristics...
                 lon_store = np.array(lon_list)
                 time_list.append(time[i])
                 time_store = np.array(time_list)
